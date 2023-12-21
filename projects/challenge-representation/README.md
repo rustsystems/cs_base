44f2ef0fa4c00b02a8fec77434d5ef9f5907b27b
Brian Abbott <briancabbott@gmail.com>
2023-12-21 14:16:23 -0800

    adding rest

---
+++ projects/challenge-representation/README.md
@@ -0,0 +1,8 @@
+
+
+What are the attributes, elements to mechanically represent a coding challenge?
+
+Sources to consider:
+   - LeetCode
+   - HackerRank
+   - Project Euler
---
+++ projects/challenge-representation/representation.py
@@ -0,0 +1,34 @@
+
+CodingStatement:
+
+
+
+===================
+WordTokenType:
+   text
+   variable
+   codingaction
+
+
+Variable
+   OuterType: Class, Container, Variable: Native, Other
+   InnerType: Int
+
+Example: 
+    ExampleInput
+    ExampleOutput
+    ExampleExplanation
+
+
+Constraints:
+   conditionalStatement: 
+
+SimiliarQuestions
+
+Topics
+   wordCloudRefs[Topic]
+
+
+CodingProblem_LeetCode:
+   description_text[WordToken]
+   examples[]
